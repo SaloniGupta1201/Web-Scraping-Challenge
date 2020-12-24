a927a05889825ef425c22ac88f370b895799adf4
SaloniGupta1201 <gupta.saloni234@gmail.com>
2020-12-23 23:29:53 -0500

    Mission to Mars

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,31 @@
+# 1. import Flask
+from flask import Flask, render_template, redirect
+import pymongo
+import scrape_mars
+
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+# 3 Mongo
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client.mars_db
+collection = db.mars
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+#mongo = pymongo(app)
+
+# 4. Define what to do when a user hits the /index route
+@app.route("/")
+def index():
+    datapage = collection.find_one()
+    return render_template("index.html", datapage=datapage)
+
+# 5. Define what to do when a user hits the /scrape route
+@app.route("/scrape")
+def scrape():
+    collection.drop()
+    scraped_data = scrape_mars.scrape_all()
+    collection.insert_one(scraped_data)
+    return redirect('/', code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
